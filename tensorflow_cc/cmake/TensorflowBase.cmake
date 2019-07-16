faa2d262dce074e133f09727de31c0eb43a861be
Rasmus Diederichsen <rasmus@psiori.com>
2019-07-16 15:29:11 +0200

    Remove protobuf download before build

--- tensorflow_cc/cmake/TensorflowBase.cmake
+++ tensorflow_cc/cmake/TensorflowBase.cmake
@@ -1,9 +1,9 @@
 cmake_minimum_required(VERSION 3.3 FATAL_ERROR)
 include(ExternalProject)
 
-# TODO This is a patch for 1.12.0.
-set(OLD_PROTOBUF "https://mirror.bazel.build/github.com/google/protobuf/archive/396336eb961b75f03b25824fe86cf6490fb75e3a.tar.gz")
-set(FIXED_PROTOBUF "https://github.com/protocolbuffers/protobuf/releases/download/v3.6.0/protobuf-all-3.6.0.tar.gz")
+if(SYSTEM_PROTOBUF)
+
+endif()
 
 ExternalProject_Add(
   tensorflow_base
@@ -16,11 +16,9 @@ ExternalProject_Add(
   BUILD_IN_SOURCE 1
   UPDATE_COMMAND ""
   CONFIGURE_COMMAND make -f tensorflow/contrib/makefile/Makefile clean
-
-            # TODO This is a patch for 1.12.0.
-            COMMAND git checkout master -- tensorflow/tf_version_script.lds
-            COMMAND sed -i "s#${OLD_PROTOBUF}#${FIXED_PROTOBUF}#g" tensorflow/contrib/makefile/download_dependencies.sh
-
+            COMMAND sed -i "134d" tensorflow/contrib/makefile/download_dependencies.sh
+            COMMAND sed -i "142,143d" tensorflow/contrib/makefile/download_dependencies.sh
+            COMMAND sed -i "42d" tensorflow/contrib/makefile/build_all_linux.sh
             COMMAND tensorflow/contrib/makefile/download_dependencies.sh
   BUILD_COMMAND ""
   INSTALL_COMMAND ""
