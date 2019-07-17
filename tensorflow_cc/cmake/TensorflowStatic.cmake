16fab29e4b317eacd0e96985d337d031ba1f3b3b
Rasmus Diederichsen <rasmus@psiori.com>
2019-07-17 14:52:09 +0200

    Foobar

--- tensorflow_cc/cmake/TensorflowStatic.cmake
+++ tensorflow_cc/cmake/TensorflowStatic.cmake
@@ -1,6 +1,7 @@
 cmake_minimum_required(VERSION 3.3 FATAL_ERROR)
 include(ExternalProject)
 
+if(SYSTEM_PROTOBUF)
 ExternalProject_Add(
   tensorflow_static
   DEPENDS tensorflow_base
@@ -10,9 +11,10 @@ ExternalProject_Add(
   BUILD_IN_SOURCE 1
   DOWNLOAD_COMMAND ""
   UPDATE_COMMAND ""
-  CONFIGURE_COMMAND tensorflow/contrib/makefile/build_all_linux.sh
+  CONFIGURE_COMMAND ${CMAKE_CURRENT_SOURCE_DIR}/cmake/build.sh
             COMMAND cp "${CMAKE_CURRENT_SOURCE_DIR}/cmake/copy_links.sh" .
             COMMAND ./copy_links.sh .
   BUILD_COMMAND ""
   INSTALL_COMMAND ""
 )
+endif()
---
+++ tensorflow_cc/cmake/build.sh
@@ -0,0 +1,1 @@
+DOWNLOAD_DEPENDENCIES=true USE_SYSTEM_PROTOBUF=true OPTFLAGS="-O0 -march=native" tensorflow/contrib/makefile/build_all_linux.sh
